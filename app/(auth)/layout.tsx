e8768d5402aa7a9c96b257ec3bb60615cd3b8d3c
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2023-08-04 13:20:37 +0000

    Created file structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: 'You\re Wrong',
+    description: 'A Next.js 13 Application'
+}
+
+export defualt function RootLayout({ 
+    children 
+}: { 
+    children: React.ReactNode 
+}) {
+  return (
+    
+    )
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>You're Wrong</h1>
+    </main>
+  )
+}
