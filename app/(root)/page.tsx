9f9b6514ef354ffc18aa9c6a3d6a90d6edf029dd
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2023-08-04 16:47:03 +0000

    Added clerk auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs"
+
 export default function Home() {
   return (
     <main>
-      <h1>You're Wrong</h1>
+      <UserButton afterSignOutUrl="/"/>
     </main>
   )
 }
