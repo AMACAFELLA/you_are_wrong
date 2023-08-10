429651f354fb05c493d01c2a84e097b9b36042ca
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2023-08-10 14:17:58 +0000

    Backend connected to mongoDB

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -22,6 +22,8 @@ import { ChangeEvent, useState } from 'react';
 import { Textarea } from '../ui/textarea';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing';
+import { updateUser } from '@/lib/actions/user.action';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Props{
     user: {
@@ -38,6 +40,8 @@ interface Props{
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([])
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation) ,
@@ -86,6 +90,20 @@ e.preventDefault();
         }
       }
       
+      await updateUser({
+        userId: user.id,
+        username: values.username,
+        name: values.name,
+        bio: values.bio,
+        image: values.profile_photo,
+        path: pathname
+      });
+      
+      if(pathname === '/profile/edit'){
+        router.back();
+      } else {
+        router.push('/');
+      }
     }
 
     return (
