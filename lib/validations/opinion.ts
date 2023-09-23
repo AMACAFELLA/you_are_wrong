193698d6776de4bec9379da56278dbf05821e1db
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2023-09-23 19:25:49 +0200

    Improved validations

--- lib/validations/opinion.ts
+++ lib/validations/opinion.ts
@@ -1,12 +1,12 @@
 import * as z from 'zod'
 
 const OpinionValidation = z.object({
-    opinion: z.string().min(40, 'Minimum 40 characters').max(500, 'Maximum 500 characters, we do not want paragraphs here'),
+    opinion: z.string().min(40, 'Minimum 40 characters').max(500, 'Maximum 500 characters, we do not want paragraphs here.'),
 
 })
 
 const DisagreementValidation = z.object({
-    disagreement: z.string().min(40, 'Minimum 40 characters, you can not just say no').max(500, 'Maximum 500 characters, we do not want paragraphs here'),
+    disagreement: z.string().min(40, 'Minimum 40 characters, you can not just say you are wrong.').max(500, 'Maximum 500 characters, we do not want paragraphs here.'),
 })
 
 export { OpinionValidation, DisagreementValidation }
