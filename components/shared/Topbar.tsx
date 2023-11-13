608e8d7c2b88151149410feaf457137f29fb1f41
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2023-11-13 06:35:36 +0200

    Updated donation button

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import LogoutComponent from "./LogoutComponent";
 import Logo from "./Logo";
+import { Button } from "../ui/button";
 const TopBar = async ({ withLogout = true }: { withLogout?: boolean }) => {
   return (
     <nav className="topbar">
@@ -13,7 +14,7 @@ const TopBar = async ({ withLogout = true }: { withLogout?: boolean }) => {
         <div className="block md:hidden">
           {withLogout ? <LogoutComponent /> : <></>}
         </div>
-        <a href="https://www.paypal.com/donate/?hosted_button_id=DJBKCVQ3KZRYY" target="_blank" rel="noopener noreferrer" className="header-gradient font-bold">
+        <a href="https://www.buymeacoffee.com/angusmacapm" target="_blank" rel="noopener noreferrer" className="header-gradient font-bold">
           Donate
         </a>
       </div>
